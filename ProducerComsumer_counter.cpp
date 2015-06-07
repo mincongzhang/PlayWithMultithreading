bb2620ae9baa8d04ef7209e5f6b97bb966796c84
Mincong <mincong.zhang@hotmail.com>
2015-06-07 19:48:14 +0100

    Create ProducerComsumer_counter.cpp

---
+++ ProducerComsumer_counter.cpp
@@ -0,0 +1,53 @@
+#include <boost/thread.hpp>
+#include <iostream>
+
+using namespace std;
+
+boost::mutex m;
+boost::condition_variable con_producer;
+boost::condition_variable con_consumer;
+volatile bool ready_to_consume = false;
+volatile bool done = false;
+int counter(0);
+
+
+void Consumer()
+{
+  boost::mutex::scoped_lock lock(m);
+  while(!done)
+    {
+      while (!ready_to_consume)
+        {
+          con_consumer.wait(lock);
+        }
+      cout << counter << endl;
+      ready_to_consume = false;
+      con_producer.notify_one();
+    }
+}
+
+void Producer()
+{
+  while(!done)
+    {
+      boost::this_thread::sleep(boost::posix_time::millisec(500));
+      boost::mutex::scoped_lock lock(m);
+      while(ready_to_consume)
+        {
+          con_producer.wait(lock);
+        }
+      ++counter;
+      ready_to_consume = true;
+      done = (counter == 10);
+      con_consumer.notify_one();
+    }
+}
+
+int main()
+{
+  boost::thread consumer_pthread(Consumer);
+  boost::thread producer_pthread(Producer);
+  consumer_pthread.join();
+  producer_pthread.join();
+  return 0;
+}
