c760831a2675974b2a02ef6a1fd6bffb7f486336
Mincong <mincong.zhang@hotmail.com>
2015-07-17 09:35:56 +0100

    no deadlock

--- WaitAndDo_ForCpuPlayer.cpp
+++ WaitAndDo_ForCpuPlayer.cpp
@@ -7,15 +7,20 @@ using namespace std;
 boost::mutex m;
 boost::condition_variable con_publisher;
 int counter(0);
-
+volatile bool done = false;
 //Publish for every 1000 ms, prevent producer producing too fast
 //However when producer produces kind of slow, wait for it to finish
 void Publisher()
 {
-  boost::mutex::scoped_lock lock(m);
+
   boost::this_thread::sleep(boost::posix_time::millisec(1000));
-  con_publisher.wait(lock);
+  cout <<"Waited: [1000] ms"<< endl;
+  if(!done){//if done, just skip this lock and no waiting
+    boost::mutex::scoped_lock lock(m);
+    con_publisher.wait(lock);
+  }
   cout <<"Publish production: ["<<counter<<"]"<< endl;
+  done = false;
 }
 
 void Producer()
@@ -23,8 +28,9 @@ void Producer()
   for(int i=0;i<10;i++){
     ++counter;
     cout <<"Producing: ["<<counter<<"]"<< endl;
-    boost::this_thread::sleep(boost::posix_time::millisec(rand() % 100 + 300));
+    boost::this_thread::sleep(boost::posix_time::millisec(rand() % 100 + 300));//may have deadlock when Producer product too quick
   }
+  done = true;
   con_publisher.notify_one();
 }
 
