9a9a0d75574f64e5fa1fb0e413f4ba02a5d65f9b
Mincong <mincong.zhang@hotmail.com>
2017-08-24 15:20:55 +0100

    Update ShmHashMap_string.cpp

--- SharedMem/ShmHashMap_string.cpp
+++ SharedMem/ShmHashMap_string.cpp
@@ -23,16 +23,16 @@ int main (int argc, char *argv[])
   typedef std::pair<const KeyType, MappedType> ValueType;
   typedef boost::interprocess::allocator<ValueType, boost::interprocess::managed_shared_memory::segment_manager> ShmAlloc;
   typedef boost::unordered_map<KeyType, MappedType, boost::hash<KeyType>, std::equal_to<KeyType>, ShmAlloc> ShmHashMap;
-  boost::interprocess::managed_shared_memory segment(boost::interprocess::open_or_create, "ContainerSharedMemory", 65535);
+  boost::interprocess::managed_shared_memory segment(boost::interprocess::open_or_create, "MizhangSharedMemory", 65535);
   ShmHashMap *hash_map1 = segment.find_or_construct<ShmHashMap>("ShmHashMap1")(3, boost::hash<KeyType>(), std::equal_to<KeyType>(), segment.get_allocator<ValueType>());
 
   for(int i = 0; i < 500; ++i) {
-    ShmHashMap::iterator iter = hash_map1->find(ShmString("1",segment.get_allocator<ShmString>()));
+    ShmHashMap::iterator iter = hash_map1->find(ShmString(to_string(i).c_str(),segment.get_allocator<ShmString>()));
     if (iter != hash_map1->end()) {
       std::cout << "[ShmHashMap1]<" << i << ", " << iter->second << ">" << std::endl;
       iter->second = ShmString(to_string(i).c_str(),segment.get_allocator<ShmString>());
     } else {
-      hash_map1->insert(ValueType(ShmString(to_string(i).c_str(),segment.get_allocator<ShmString>()), ShmString("1",segment.get_allocator<ShmString>())));
+      hash_map1->insert(ValueType(ShmString(to_string(i).c_str(),segment.get_allocator<ShmString>()), ShmString("init empty",segment.get_allocator<ShmString>())));
     }
   }
 
